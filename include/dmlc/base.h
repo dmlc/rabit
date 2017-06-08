8fc2b6a5f69030a94daab4cb1f904e1cf0c01f6c
nijie <nijie1016@gmail.com>
2017-06-08 22:32:33 +1000

    Fix lint error

--- include/dmlc/base.h
+++ include/dmlc/base.h
@@ -41,7 +41,8 @@
  * \brief Wheter to print stack trace for fatal error,
  * enabled on linux when using gcc.
  */
-#if (!defined(DMLC_LOG_STACK_TRACE) && defined(__GNUC__) && !defined(__MINGW32__) && !defined(__CYGWIN__))
+#if (!defined(DMLC_LOG_STACK_TRACE) && defined(__GNUC__) && !defined(__MINGW32__) \
+    && !defined(__CYGWIN__))
 #define DMLC_LOG_STACK_TRACE 1
 #endif
 
