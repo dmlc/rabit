f19d62ecb249a5f875c9946f93f0b3b6395045e2
Chen Qin <cq@uber.com>
2019-04-09 13:35:25 -0700

    port packages from xgb

--- scripts/travis_script.sh
+++ scripts/travis_script.sh
@@ -32,38 +32,10 @@ if [ ${TASK} == "cmake-build" ]; then
     make all || exit -1
 fi
 
-if [ ${TASK} == "xgb-cmake" ]; then
-    git clone --recursive https://github.com/chenqin/xgboost ../xgboost
-    rm -rf ../xgboost/rabit
-    ln -s ../rabit ../xgboost/rabit
-    cd ../xgboost
-    source tests/travis/setup.sh
-    set -e
-    # Build gtest via cmake
-    wget -nc https://github.com/google/googletest/archive/release-1.7.0.zip
-    unzip -n release-1.7.0.zip
-    mv googletest-release-1.7.0 gtest && cd gtest
-    cmake . && make
-    mkdir lib && mv libgtest.a lib
-    cd ..
-    rm -rf release-1.7.0.zip
-
-    # Build/test
-    rm -rf build
-    mkdir build && cd build
-    PLUGINS="-DPLUGIN_LZ4=ON -DPLUGIN_DENSE_PARSER=ON"
-    cmake .. -DGOOGLE_TEST=ON -DGTEST_ROOT=$PWD/../gtest/ ${PLUGINS}
-    make
-    ./testxgboost
-    cd ..
-    rm -rf build
-fi
-
 if [ ${TASK} == "xgb-java-tests" ]; then
     git clone --recursive https://github.com/chenqin/xgboost ../xgboost
     rm -rf ../xgboost/rabit
     ln -s ../rabit ../xgboost/rabit
-    echo "MAVEN_OPTS='-Xmx2g -XX:MaxPermSize=1024m -XX:ReservedCodeCacheSize=512m -Dorg.slf4j.simpleLogger.defaultLogLevel=error'" > ~/.mavenrc
     cd ../xgboost
     source tests/travis/setup.sh
 
